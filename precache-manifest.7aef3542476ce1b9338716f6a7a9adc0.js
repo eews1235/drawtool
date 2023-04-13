d289705fe95ce45f98a2f60c4b9ba2fed0d4727d
eews1235 <130626190+eews1235@users.noreply.github.com>
2023-04-13 13:48:11 +0900

    Add files via upload

---
+++ precache-manifest.7aef3542476ce1b9338716f6a7a9adc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac1c251b075824b02bde",
+    "url": "/drawtool/static/css/main.0670acc1.chunk.css"
+  },
+  {
+    "revision": "ac1c251b075824b02bde",
+    "url": "/drawtool/static/js/main.ac1c251b.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/drawtool/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/drawtool/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "46556c3600993566a114",
+    "url": "/drawtool/static/js/runtime~main.46556c36.js"
+  },
+  {
+    "revision": "512227835da1fc65f9a663cc03165d01",
+    "url": "/drawtool/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6d999396c242eaefc50990be5ca7ace.js"
+  "/drawtool/precache-manifest.7aef3542476ce1b9338716f6a7a9adc0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/drawtool/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
